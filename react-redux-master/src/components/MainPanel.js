51b72ca55756e56a3592deb13a616b96ba349a68
santhiago5600 <santhiagoac97@gmail.com>
2024-06-26 23:26:58 -0400

    redux proyect

--- react-redux-master/src/components/MainPanel.js
+++ react-redux-master/src/components/MainPanel.js
@@ -1,1 +1,12 @@
+import React from "react";
+import MyButon from "./MyButton";
+import DivPanel from "./DivPanel";
 
+const MainPanel = ()=>{
+    return (
+        <div>
+            This is a main panel <MyButon></MyButon><DivPanel></DivPanel>
+        </div>
+    )
+}
+export default MainPanel;
